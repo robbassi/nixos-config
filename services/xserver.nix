d2976d6181a2da4221b28d63f52e750bf739a228
Rob Bassi <rob.bassi.89@gmail.com>
2024-04-27 19:47:41 -0400

    Fix caps remap for wayland

--- services/xserver.nix
+++ services/xserver.nix
@@ -5,9 +5,14 @@
     enable = true;
 
     displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+    desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverrides = ''
+        [org.gnome.desktop.input-sources]
+        xkb-options=['caps:ctrl_modifier']
+      '';
+    };
 
     layout = "us";
-    xkbOptions = "caps:ctrl_modifier";
   };
 }
