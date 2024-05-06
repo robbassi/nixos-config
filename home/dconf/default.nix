6112c77e7312774db1cd9f07ab002bbae9170f01
Rob Bassi <rob.bassi.89@gmail.com>
2024-05-06 18:20:39 -0400

    Add pinned apps to dconf config

--- home/dconf/default.nix
+++ home/dconf/default.nix
@@ -5,6 +5,14 @@
       "org/gnome/desktop/input-sources" = {
         xkb-options = ["caps:ctrl_modifier"];
       };
+      "org/gnome/shell" = {
+        favorite-apps = [
+          "google-chrome.desktop"
+          "kitty.desktop"
+          "org.gnome.Nautilus.desktop"
+          "gnome-system-monitor.desktop"
+        ];
+      };
     };
   };
 }
